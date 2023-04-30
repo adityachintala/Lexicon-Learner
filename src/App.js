5c7b4ab330d27aeeb5ab4a9c3d56771e42628253
adityachintala <adityachintala27@gmail.com>
2023-04-30 16:23:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.css';
+
+function App() {
+  return (
+    <nav class = "navbar navbar-light bg-light" role="navigation">
+      <div class = "navbar-header">
+        <a class = "navbar-brand" href = "https://github.com/adityachintala">Lexicon Learner</a>
+      </div>
+    </nav>
+  );
+}
+
+export default App;
